f227ffff6151f0357e2dc80c22aee533816ab1d9
Justin Parker <jparkerweb@gmail.com>
2014-08-17 18:08:13 -0700

    add col-no-class; update demo

--- gridline-marker-helper.js
+++ gridline-marker-helper.js
@@ -1,10 +1,16 @@
 $( document ).ready(function() {
+	
 	// bind gridlines toggle
 	$(".gridlines-toggle").on("click", function () {
 		$(".gridlines").toggle();
 	});
+	// toggle off gridlines on page load
+	$(".gridlines").toggle();
+
 	// bind markers toggle
 	$(".markers-toggle").on("click", function () {
 		$(".marker").toggle();
 	});
+	// toggle off markers on page load
+	$(".marker").toggle();
 });
