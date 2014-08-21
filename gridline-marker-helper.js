093449f9a8bf377c1a8c18e5e0b9a3525693d972
Justin Parker <jparkerweb@gmail.com>
2014-08-21 06:26:41 -0700

    update demo

--- gridline-marker-helper.js
+++ gridline-marker-helper.js
@@ -1,5 +1,46 @@
 $( document ).ready(function() {
-	
+
+	// define markup require to render Gridline & Marker Helper elements
+	var gridlinesAndMarkers = " \
+		<!-- \
+			******************************************************************* \
+			** Helper Markup - Gridline/Breakpoint Marker Overlay & Controls ** \
+			******************************************************************* \
+		--> \
+		<!-- gridlines --> \
+		<div class=\"gridlines\"> \
+			<div class=\"grid\"> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">1</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">2</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">3</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">4</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">5</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">6</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">7</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">8</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">9</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">10</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">11</div></div> \
+				<div class=\"col-1\"><div class=\"gridlines--col\">12</div></div> \
+			</div> \
+		</div> \
+		<!-- markers for grid --> \
+		<div class=\"marker marker--stack\"></div> \
+		<div class=\"marker marker--xs\"></div> \
+		<div class=\"marker marker--s\"></div> \
+		<div class=\"marker marker--m\"></div> \
+		<div class=\"marker marker--l\"></div> \
+		<div class=\"marker-indicator\"></div> \
+		<!-- gridlines toggle --> \
+		<div class=\"gridlines-toggle\">gridlines</div> \
+		<!-- markers toggle --> \
+		<div class=\"markers-toggle\">markers</div>	\
+		<!-- ******************************************************************* --> \
+	";
+
+	// insert Gridline and Marker elements
+	$("body").append(gridlinesAndMarkers);
+
 	// bind gridlines toggle
 	$(".gridlines-toggle").on("click", function () {
 		$(".gridlines").toggle();
@@ -14,4 +55,5 @@ $( document ).ready(function() {
 	});
 	// toggle off markers on page load
 	$(".marker").toggle();
+	
 });
